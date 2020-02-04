894388ffe1905b5e9a1c8f055b741e287cf96621
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2020-02-04 17:58:25 +0100

    Reduces few lines

--- day05/ex04/ft_strncpy.c
+++ day05/ex04/ft_strncpy.c
@@ -12,13 +12,7 @@
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	while (n--)
+		dest++ = src++;
 	return (dest);
 }

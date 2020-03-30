bc83308cc39ebfc1b47bcb8314dfdc46fe1c5798
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2020-03-30 18:37:44 +0100

    Cleans the dirt

--- day05/ex07/ft_strncmp.c
+++ day05/ex07/ft_strncmp.c
@@ -12,15 +12,11 @@
 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (n && *s1 && (*s1 == *s2))
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		++s1;
+		++s2;
+		--n;
 	}
-	return (0);
+	return (n == 0 ? 0 : *s1 - *s2);
 }
